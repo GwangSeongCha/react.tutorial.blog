3d61db2eca841c74671c8eb8bae2c2b8599d4068
GwangSeongCha <uhon0425@naver.com>
2021-09-11 23:55:48 +0900

    9.11 차광성 / 리액트 블로그 2일차

--- src/App.js
+++ src/App.js
@@ -6,46 +6,42 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
-  let posts = '강남 고기 맛집';
-
-  function 제목바꾸기 (){
-    var newArray = [...글제목];
-    newArray[0] = '여자코트 추천';
-    글제목변경(newArray);
-  }
+  let [modal, modal변경] = useState(true);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
-      <div className="list">
-        <h3>{ 글제목[0] } <span onClick={ ()=> { 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{ 글제목[1] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{ 글제목[2] }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
+      
+      {
+        글제목.map(function(글){
+          return (
+          <div className="list">
+            <h3>{ 글 } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
+            <p>2월 18일 발행</p>
+            <hr/>
+          </div>
+          )
+        })
+      }
 
-      <Modal></Modal>
+      <button onClick={ ()=>{modal변경(!modal)} }>열고닫기</button>
+
+      {
+        modal === true
+        ? <Modal 글제목={글제목} ></Modal>
+        : null
+      }
 
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2> { props.글제목[0] } </h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
