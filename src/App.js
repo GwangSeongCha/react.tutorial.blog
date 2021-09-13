ddc7f11e60e496cc57cca2c4a8358aeaddc69361
GwangSeongCha <uhon0425@naver.com>
2021-09-13 15:24:29 +0900

    9.13 차광성 / 리액트 블로그 4일차

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(true);
+  let [누른제목, 누른제목변경] = useState(0);
+
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -15,10 +18,10 @@ function App() {
       </div>
       
       {
-        글제목.map(function(글){
+        글제목.map(function(글, i){
           return (
-          <div className="list">
-            <h3>{ 글 } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
+          <div className="list" key={i}>
+            <h3 onClick={ ()=>{ 누른제목변경(i) } }>{ 글 } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
             <p>2월 18일 발행</p>
             <hr/>
           </div>
@@ -26,11 +29,20 @@ function App() {
         })
       }
 
+      <div className="publish">
+        <input onChange={ (e)=>{입력값변경(e.target.value) } }></input>
+        <button onClick={ ()=>{
+          var arrayCopy = [...글제목];
+          arrayCopy.unshift(입력값);
+          글제목변경( arrayCopy );
+        } }>저장</button>
+      </div>
+
       <button onClick={ ()=>{modal변경(!modal)} }>열고닫기</button>
 
       {
         modal === true
-        ? <Modal 글제목={글제목} ></Modal>
+        ? <Modal 글제목={글제목} 누른제목={누른제목} ></Modal>
         : null
       }
 
@@ -41,9 +53,9 @@ function App() {
 function Modal(props){
   return (
     <div className="modal">
-      <p> { props.글제목[0] } </p>
-      <p> { props.글제목[1] }</p>
-      <p> { props.글제목[2] }</p>
+      <h2> { props.글제목[props.누른제목] } </h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   )
 }
